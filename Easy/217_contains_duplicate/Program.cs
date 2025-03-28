ec2f6c7b4815e628d2bd1844ade5cb38927df619
Shawn Kuo <shawn_kuo@pxpayplus.com>
2025-03-28 14:31:22 +0800

    contains_duplicate

---
+++ Easy/217_contains_duplicate/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+int[] nums1 = { 1, 2, 3, 3 };
+int[] nums2 = { 1, 2, 3, 4 };
+
+var solution = new Solution();
+Console.WriteLine($"[1, 2, 3, 3] → {solution.hasDuplicate(nums1)}");
+Console.WriteLine($"[1, 2, 3, 4] → {solution.hasDuplicate(nums2)}");
